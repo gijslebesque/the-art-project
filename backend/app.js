c9c453807db7f44826c19c2e1b3fb082005c3a6c
Gijs Lebesque <gijslebesque@gmail.com>
2019-01-04 22:53:25 +0100

    setting up passport

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const config = require('./config/config.json');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const LocalStrategy = require('passport-local').Strategy;
+const session    = require('express-session');
+const passport     = require('passport');
+
+require('./config/passport.js');
+
+mongoose.connect('mongodb://localhost:27017/art-db', (err) => {
+    if(err) console.log(err)
+    else console.log("Connected")
+});
+
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if(config.environment === "development"){
+	app.set('views', path.join(__dirname, 'views'));
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(require("cors")({
+        credentials: true,
+        origin: ["http://localhost:3000", "http://localhost:3000/", "localhost:3000/", "http://localhost:3001"]
+	}));
+	app.get("/", (req, res) =>{
+		res.send("pong")
+	})
+}
+if(config.environment === "production") {	
+    app.use(express.static(path.join(__dirname, 'client')));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '/client', 'index.html'));
+    })   
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+const { Schema } = mongoose;
+
+const UsersSchema = new Schema({
+  email: String,
+  password: String
+});
